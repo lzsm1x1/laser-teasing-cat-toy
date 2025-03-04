383d143ff7b23e2237c823deedb9562601a3de23
lzsm1x1 <904173965@qq.com>
2025-03-04 22:20:11 +0800

    v0.2
    v0.2 use 2 motor to control 1 laser module， but these 2 motor are controled by the same pin GIO2(D4)

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -12,12 +12,18 @@
 
   I hope that I can make a laser teasing cat toy.
 
+  
+
   v0.1 This is a test project, to test the NodeMCU , motor and serial is ok. 
 */
 
 #include <Servo.h>
 
+#define MOTOR_RUN  2
+#define MOTOR_STOP 1
+
 int pos = 0;
+char comm_start = MOTOR_RUN;
 Servo myservo;                            // create servo object to control a servo
                                           // twelve servo objects can be created on most boards
 void setup() {
@@ -25,17 +31,40 @@ void setup() {
   myservo.attach(2);                      // attaches the servo on GIO2(D4) to the servo object
   Serial.begin(9600);
   Serial.println("ok");
+
+
 }
 void loop() {
-  Serial.println("in loop");
+  // comm_start = Serial.read();
+  // Serial.println(comm_start);
+  if(comm_start == MOTOR_STOP)
+  {
+    motor_stop();
+  }
+  else
+  {
+    motor_run();
+  }
+
+}
+
+void motor_run()
+{
+  Serial.println("motor_run");
   for (pos = 0; pos <= 180; pos += 1) {   // goes from 0 degrees to 180 degrees
     myservo.write(pos);                   // tell servo to go to position in variable 'pos'
-    Serial.println(pos);                  // waits 15ms for the servo to reach the position
+    //Serial.println(pos);                  // waits 15ms for the servo to reach the position
     delay(15);
   }
   for (pos = 180; pos >= 0; pos -= 1) {   // goes from 180 degrees to 0 degrees
     myservo.write(pos);
-    Serial.println(pos);                  // tell servo to go to position in variable 'pos'
+    //Serial.println(pos);                  // tell servo to go to position in variable 'pos'
     delay(15);                            // waits 15ms for the servo to reach the position
   }
+}
+
+void motor_stop()
+{
+  //Serial.println("motor_stop");
+    myservo.write(pos);                   // tell servo to go to position in variable 'pos'
 }

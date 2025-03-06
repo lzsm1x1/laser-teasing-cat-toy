73179d9d80c91edd7404ee5d6275229ff3f55f36
lzsm1x1 <904173965@qq.com>
2025-03-06 23:23:26 +0800

    v0.3
    v0.3 1.Add a function witch is called "way1", this simple algorithm allows two servos to control the laser move like sawtooth wave.
                    2.Add #define name rule, update the name of definition.

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -13,58 +13,127 @@
   I hope that I can make a laser teasing cat toy.
 
   
+                2.Add #define name rule, update the name of definition.
 
-  v0.1 This is a test project, to test the NodeMCU , motor and serial is ok. 
-*/
+  #define name rule : [item]_[type]_[category]_[Detial]
+              instance: SERVO_CMMD_SYSTEM_STOP
+                        LASER_CMMD_SWITCH_ON
+
+  function name rule : []
+
+  variable name rule : []
+  */
 
 #include <Servo.h>
 
-#define MOTOR_RUN  2
-#define MOTOR_STOP 1
+#define SERVO_CMMD_SYSTEM_STOP  0
+#define SERVO_CMMD_SYSTEM_RUN 1
+#define SERVO_DEGREE_HORIZON_MAX 180
+#define SERVO_DEGREE_HORIZON_MIN 0
+#define SERVO_DEGREE_VERTICAL_MAX 180
+#define SERVO_DEGREE_VERTICAL_MIN 0
 
-int pos = 0;
-char comm_start = MOTOR_RUN;
-Servo myservo;                            // create servo object to control a servo
+int pos_horizon = SERVO_DEGREE_HORIZON_MIN;
+int pos_vertical = SERVO_DEGREE_VERTICAL_MIN;
+
+char servo_cmmd = SERVO_CMMD_SYSTEM_RUN;
+Servo servo_horizon;                            // create servo object to control a servo
+Servo servo_vertical;
                                           // twelve servo objects can be created on most boards
 void setup() {
-  // myservo.attach(9, 500, 2500);
-  myservo.attach(2);                      // attaches the servo on GIO2(D4) to the servo object
+  // servo_horizon.attach(9, 500, 2500);
+  servo_horizon.attach(2);                      // attaches the servo on GIO2(D4) to the servo object
+  servo_vertical.attach(0);                     // attaches the servo on GIO0(D3) to the servo object
   Serial.begin(9600);
   Serial.println("ok");
 
 
 }
 void loop() {
-  // comm_start = Serial.read();
-  // Serial.println(comm_start);
-  if(comm_start == MOTOR_STOP)
+  // servo_cmmd = Serial.read();
+  // Serial.println(servo_cmmd);
+  if(servo_cmmd == MOTOR_CMMD_SYSTEM_STOP)
   {
-    motor_stop();
+    servo_stop();
   }
-  else
+  else if(servo_cmmd == MOTOR_CMMD_SYSTEM_RUN)
   {
-    motor_run();
+    way1();
+    // servo_run_horizon();
+    // servo_run_vertical();
+  }else{
+    delay(20);
+    //Do no thing
   }
 
 }
 
-void motor_run()
+void servo_run_horizon()
 {
-  Serial.println("motor_run");
-  for (pos = 0; pos <= 180; pos += 1) {   // goes from 0 degrees to 180 degrees
-    myservo.write(pos);                   // tell servo to go to position in variable 'pos'
-    //Serial.println(pos);                  // waits 15ms for the servo to reach the position
+  Serial.println("servo_run_horizon");
+  for (pos_horizon = SERVO_DEGREE_HORIZON_MIN; pos_horizon <= SERVO_DEGREE_HORIZON_MAX; pos_horizon += 1) {   // goes from 0 degrees to 180 degrees
+    servo_horizon.write(pos_horizon);                   // tell servo to go to pos_horizonition in variable 'pos_horizon'
+    //Serial.println(pos_horizon);                  // waits 15ms for the servo to reach the pos_horizonition
     delay(15);
   }
-  for (pos = 180; pos >= 0; pos -= 1) {   // goes from 180 degrees to 0 degrees
-    myservo.write(pos);
-    //Serial.println(pos);                  // tell servo to go to position in variable 'pos'
-    delay(15);                            // waits 15ms for the servo to reach the position
+  for (pos_horizon = SERVO_DEGREE_HORIZON_MAX; pos_horizon >= SERVO_DEGREE_HORIZON_MIN; pos_horizon -= 1) {   // goes from 180 degrees to 0 degrees
+    servo_horizon.write(pos_horizon);
+    //Serial.println(pos_horizon);                  // tell servo to go to pos_horizonition in variable 'pos_horizon'
+    delay(15);                            // waits 15ms for the servo to reach the pos_horizonition
   }
 }
 
-void motor_stop()
+void servo_run_vertical()
 {
-  //Serial.println("motor_stop");
-    myservo.write(pos);                   // tell servo to go to position in variable 'pos'
+  Serial.println("servo_run_horizon");
+  for (pos_vertical = SERVO_DEGREE_HORIZON_MIN; pos_vertical <= SERVO_DEGREE_VERTICAL_MAX; pos_vertical += 1) {   // goes from 0 degrees to 180 degrees
+    servo_vertical.write(pos_vertical);                   // tell servo to go to pos_horizonition in variable 'pos_horizon'
+    //Serial.println(pos_vertical);                  // waits 15ms for the servo to reach the pos_horizonition
+    delay(15);
+  }
+  for (pos_vertical = SERVO_DEGREE_VERTICAL_MAX; pos_vertical >= SERVO_DEGREE_HORIZON_MIN; pos_vertical -= 1) {   // goes from 180 degrees to 0 degrees
+    servo_vertical.write(pos_vertical);
+    //Serial.println(pos_vertical);                  // tell servo to go to pos_horizonition in variable 'pos_horizon'
+    delay(15);                            // waits 15ms for the servo to reach the pos_horizonition
+  }
+}
+
+void way1()
+{
+  int toward = 1; //define the diraction of servo
+  //The horizon diraction of servo is moved as normal(1 degree per cycle), vertical servo is performs reciprocating motion at an angle of 0-30 degrees(2 degree per cycle)
+  for (pos_horizon = SERVO_DEGREE_HORIZON_MIN; pos_horizon <= SERVO_DEGREE_HORIZON_MAX; pos_horizon += 1) {   
+    if(toward == 1)
+    {
+          if(pos_vertical <= 30 && pos_vertical >= 0){
+            pos_vertical += 2;
+          }else
+          {
+            toward = 0;
+          }
+    }else
+    {
+          if(pos_vertical != 0){
+            pos_vertical -= 2;
+          }else
+          {
+            toward = 1;
+          }
+    }
+
+    servo_vertical.write(pos_vertical);
+    servo_horizon.write(pos_horizon);                   // tell servo to go to pos_horizonition in variable 'pos_horizon'
+    // Serial.println("pos_horizon"); 
+    // Serial.println(pos_horizon);                  
+    // Serial.println("pos_vertical");
+    // Serial.println(pos_vertical);
+    delay(15);
+  }
+}
+
+void servo_stop()
+{
+  //Serial.println("MOTOR_CMMD_SYSTEM_STOP");
+    servo_horizon.write(SERVO_DEGREE_HORIZON_MIN);                   // tell servo to go to degree '0'
+    servo_vertical.write(SERVO_DEGREE_VERTICAL_MIN);
 }
